120483b4daab6c8aa6f06ef2f4680b46fec04be7
rolansy <ronalshoeygeorge@gmail.com>
2024-02-27 09:48:44 +0530

    Add script to find the second largest number in a list

---
+++ second.sh
@@ -0,0 +1,17 @@
+read -p "Enter Number of terms : " n
+l=0
+sl=0
+for((i=1;i<=n;i++)); do
+	read -p "Enter Number $i : " x
+	if [ $x -gt $l ]; then
+		sl=$l
+		l=$x		
+	elif [ $x -gt $sl ] && [ $x -ne $l ]; then
+		sl=$x
+	fi
+done
+
+echo "Second Largest Number is : $sl"
+
+echo exit
+
