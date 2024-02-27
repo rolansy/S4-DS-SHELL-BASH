24150183af9db72b1ea9ee9dde67815621f7c339
rolansy <ronalshoeygeorge@gmail.com>
2024-02-27 09:48:16 +0530

    Add Fibonacci series script

---
+++ fibonacci.sh
@@ -0,0 +1,19 @@
+read -p "Enter Number of terms : " n
+
+fib(){
+	n=$1
+	a=0
+	b=1
+	for ((i=0; i<$n; i++)); do
+		t=$(($a+$b))
+		echo -n "$t "
+		a=$b
+		b=$t
+	done
+	
+}
+
+echo -n "Fibonacci series upto $n terms : "
+fib $n
+echo
+exit
